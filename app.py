c13e22c3f952db851f7755c51d68529c4361bf94
unknown <to_5823_mo@yahoo.co.jp>
2024-07-08 18:20:31 +0900

    add_graph

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import yfinance as yf
+import altair as alt
+
+st.title('米国株価可視化アプリ')
+st.sidebar.write("""
+# GAFA株価
+こちらは株価可視化ツールです。以下のオプションから表示日数を指定できます。
+""")
+
+st.sidebar.write("""
+## 表示日数選択
+""")
+
+
+
+days = st.sidebar.slider('日数',  1, 5, 60)
+
+st.write(f"""
+### 過去 **{days}日間** のGAFA株価
+""")
 
 tickers = {
     'apple':'AAPL',
@@ -15,7 +33,6 @@ def get_data(tickers):
     df = pd.DataFrame()
     for company in tickers.keys():
         tkr = yf.Ticker(tickers[company])
-        days = 5 #何日分のデータを取得するか
         hist = tkr.history(period=f'{days}d')
         print(hist)
         hist.index = hist.index.strftime('%d %B %Y')
@@ -27,3 +44,27 @@ def get_data(tickers):
     return df
 
 st.write(get_data(tickers))
+
+companies = ['apple', 'facebook']
+df = get_data(tickers)
+data = df.loc[companies] #locで[ ]内に指定した条件で情報を抽出できる
+data.sort_index() #アルファベット順にソート
+data = data.T.reset_index()
+data.head()
+data = pd.melt(data, id_vars=['Date']).rename(
+    columns={'value': 'Stock Prices(USD)'}
+) #pandasのmelt関数は雑然としたデータを'Date'を基準に整然してくれる
+st.write(data)
+
+ymin=200
+ymax=300
+chart = (
+    alt.Chart(data)
+    .mark_line(opacity=0.8, clip=True)
+    .encode(
+        x="Date:T",
+        y=alt.Y("Stock Prices(USD):Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
+        color='Name:N'
+    )
+)
+st.write(chart)
